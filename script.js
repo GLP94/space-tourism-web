3b54a84430d53bf2dd31db30f01cebd252e6a3d7
Giovanni Luca Pinelli <giovannilucapinelli94@gmail.com>
2026-03-06 22:19:20 +0100

    First attempt as home page, for mobile (>425px); Need to figure out why there is white space on the bottom

---
+++ script.js
@@ -0,0 +1,14 @@
+const closeBtn = document.getElementById("close");
+const hamburger = document.getElementById("hamburger");
+const menu = document.querySelector(".menu");
+
+function onClose() {
+    menu.classList.add("hidden");
+}
+
+function onOpen() {
+    menu.classList.remove("hidden");
+}
+
+closeBtn.addEventListener("click", onClose);
+hamburger.addEventListener("click", onOpen);
